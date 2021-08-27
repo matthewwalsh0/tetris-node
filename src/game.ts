d14b3e8ceae685e202f5c195e4aec04dcc5d2cfa
Matthew Walsh <matthewwalsh0@googlemail.com>
2021-08-27 23:47:32 +0100

    Handle input using readline and stdin raw mode
    Display colours on pieces using chalk module.

--- src/game.ts
+++ src/game.ts
@@ -1,3 +1,6 @@
+import chalk from 'chalk';
+import readline from 'readline';
+
 import {Piece} from './piece';
 import {Tiles} from './tiles';
 
@@ -6,8 +9,32 @@ const HEIGHT: number = 24;
 const INTERVAL_FRAME = 16;
 const INTERVAL_GRAVITY: number = 62 * INTERVAL_FRAME;
 const INTERVAL_FPS: number = 1000;
+const INTERVAL_INPUT: number = 3 * INTERVAL_FRAME;
 const ROW_SCORES: number[] = [0, 40, 100, 300, 1200];
 const GRAVITY_SCORE: number = 1;
+
+const PIECE_COLOURS: Function[] = [
+  chalk.white.bgWhite, chalk.cyan.bgCyanBright, chalk.blue.bgBlue,
+  chalk.rgb(255, 165, 0).bgRgb(255, 165, 0), chalk.yellow.bgYellow,
+  chalk.green.bgGreen, chalk.rgb(128, 0, 128).bgRgb(128, 0, 128),
+  chalk.red.bgRed
+];
+
+class PendingInput {
+  xChange: number = 0;
+  yChange: number = 0;
+  rotateRight: boolean = false;
+  rotateLeft: boolean = false;
+
+  constructor(
+      xChange: number, yChange: number, rotateRight: boolean,
+      rotateLeft: boolean) {
+    this.xChange = xChange;
+    this.yChange = yChange;
+    this.rotateRight = rotateRight;
+    this.rotateLeft = rotateLeft;
+  }
+}
 
 export class Game {
   tiles: Tiles;
@@ -17,6 +44,8 @@ export class Game {
   frameCount: number;
   score: number;
   fps: number;
+  end: boolean;
+  pendingInput?: PendingInput;
 
   constructor() {
     this.tiles = new Tiles(WIDTH, HEIGHT);
@@ -26,45 +55,85 @@ export class Game {
     this.frameCount = 0;
     this.score = 0;
     this.fps = 0;
+    this.end = false;
   }
 
   async start() {
-    Game.setIntervalFactoringDuration(() => {
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+    process.stdin.on('keypress', (str, key) => {
+      if (this.pendingInput) return;
+
+      if (key.ctrl && key.name === 'c') {
+        process.exit();
+      } else if (key.name === 'right' || key.name === 'd') {
+        this.pendingInput = new PendingInput(1, 0, false, false);
+      } else if (key.name === 'left' || key.name === 'a') {
+        this.pendingInput = new PendingInput(-1, 0, false, false);
+      } else if (key.name === 'down' || key.name === 's') {
+        this.pendingInput = new PendingInput(0, 1, false, false);
+      } else if (key.name === 'e') {
+        this.pendingInput = new PendingInput(0, 0, true, false);
+      } else if (key.name === 'q') {
+        this.pendingInput = new PendingInput(0, 0, false, true);
+      }
+    });
+
+    this.setIntervalFactoringDuration(() => {
+      if (this.pendingInput) {
+        this.tryMove(
+            this.pendingInput.xChange, this.pendingInput.yChange,
+            this.pendingInput.rotateRight, this.pendingInput.rotateLeft);
+        this.pendingInput = undefined;
+      }
+    }, INTERVAL_INPUT);
+
+    this.setIntervalFactoringDuration(() => {
       this.tryMove(0, 1, false, false);
       this.score += GRAVITY_SCORE;
     }, INTERVAL_GRAVITY);
 
-    Game.setIntervalFactoringDuration(() => {
+    this.setIntervalFactoringDuration(() => {
       this.display();
       this.frameCount++;
     }, INTERVAL_FRAME);
 
-    Game.setIntervalFactoringDuration(() => {
+    this.setIntervalFactoringDuration(() => {
       this.fps = this.frameCount;
       this.frameCount = 0;
     }, INTERVAL_FPS);
   }
 
   private tryMove(
-      xChange: number, yChange: number, rotatedRight: boolean,
-      rotatedLeft: boolean): boolean {
+      xChange: number, yChange: number, rotateRight: boolean,
+      rotateLeft: boolean) {
     const originalX: number = this.currentX;
     const originalY: number = this.currentY;
     let newX: number = this.currentX + xChange;
     let newY: number = this.currentY + yChange;
 
+    if (rotateRight) {
+      this.currentPiece.rotateRight();
+    } else if (rotateLeft) {
+      this.currentPiece.rotateLeft();
+    }
+
     if (this.currentPiece.isCollision(this.tiles, newX, originalY)) {
       newX = originalX;
 
-      if (rotatedRight) {
+      if (rotateRight) {
         this.currentPiece.rotateLeft();
-      } else if (rotatedLeft) {
+      } else if (rotateLeft) {
         this.currentPiece.rotateRight();
       }
     }
 
     if (this.currentPiece.isCollision(this.tiles, newX, newY)) {
-      if (newY == 0) return false;
+      if (originalY == 0) {
+        this.end = true;
+        process.stdin.removeAllListeners();
+        return;
+      }
 
       this.currentPiece.draw(this.tiles, originalX, originalY);
       this.currentPiece = new Piece();
@@ -76,9 +145,7 @@ export class Game {
     this.currentX = newX;
     this.currentY = newY;
 
-    Game.checkFullRows(this.tiles, this.score);
-
-    return true;
+    Game.checkFullRows(this.tiles, this.score)
   }
 
   private display() {
@@ -95,7 +162,7 @@ export class Game {
 
       for (var x = 0; x < WIDTH; x++) {
         const value = displayTiles.get(x, y);
-        let output = value == 0 ? ' ' : value;
+        let output = value == 0 ? ' ' : PIECE_COLOURS[value]('#');
 
         line += output;
       }
@@ -158,15 +225,16 @@ export class Game {
     return true;
   }
 
-  private static setIntervalFactoringDuration(
-      callback: Function, interval: number) {
+  private setIntervalFactoringDuration(callback: Function, interval: number) {
+    if (this.end) return;
+
     const start = new Date();
     callback();
     const duration = Game.elapsedSince(start);
     const newInterval = duration > interval ? 0 : interval - duration;
 
     setTimeout(() => {
-      Game.setIntervalFactoringDuration(callback, interval);
+      this.setIntervalFactoringDuration(callback, interval);
     }, newInterval);
   }
 
